a13db03eb0ec8f631783c33572b2fb0b3529ea01
khang nguyen <kei@Khangs-MacBook-Air.local>
2022-03-06 23:03:31 +0700

    feat(core): Update git setting

--- questions/src/features/questions/questions.controller.spec.ts
+++ questions/src/features/questions/questions.controller.spec.ts
@@ -1,8 +1,11 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
 
 describe('QuestionsController', () => {
+  let app: NestExpressApplication;
   let controller: QuestionsController;
 
   beforeEach(async () => {
@@ -16,5 +19,29 @@ describe('QuestionsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return 200 and question list', async () => {
+    return request(app.getHttpServer())
+      .get('/api/v1/questions')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toBeDefined();
+      });
+  });
+
+  it('should return 200 and question content', async () => {
+    return request(app.getHttpServer())
+      .get('/api/v1/questions/bubble-sort')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toBeDefined();
+      });
+  });
+
+  it('should return 401 (premium content) if user not sign in', async () => {
+    return request(app.getHttpServer())
+      .get('/api/v1/questions/quick-sort')
+      .expect(401);
   });
 });
--- questions/src/main.ts
+++ questions/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { enhanceMiddlewares } from 'src/startup/middlewares';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // trust first proxy
+  app.set('trust proxy', 1);
+
+  enhanceMiddlewares(app);
+
   await app.listen(3000);
 }
 bootstrap();
