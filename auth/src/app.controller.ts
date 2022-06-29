c8a3a19892c11e52a3476532884af4676bd2d361
khang nguyen <kei@Khangs-MacBook-Air.local>
2022-06-29 22:42:45 +0700

    feat(BREAKING): clean project

--- auth/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Body, Controller, Get, NotFoundException, Post } from '@nestjs/common';
-import { SignUpReqDto } from 'src/features/auth/dto/signUpReq.dto';
-import { TrimStringPipe } from 'src/pipes';
-import { AppService } from './app.service';
-
-@Controller()
-export class AppController {
-  constructor() {}
-}
--- client-next/src/common/components/header/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Logo = () => {
-  return <div>Code Expert</div>;
-};
-
-export default Logo;
