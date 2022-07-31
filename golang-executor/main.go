ed3eab36688838be7fa2a01b96de53e6f2e1356e
khang nguyen <keirames.l3@gmail.com>
2022-07-31 23:03:29 +0700

    feat: get log from internal

--- golang-executor/main.go
+++ golang-executor/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 )
 
 func UNUSED(x ...interface{}) {}
@@ -23,25 +25,45 @@ func main() {
 		panic(err)
 	}
 
-	start := time.Now()
+	timingCreateContainer := time.Now()
 	res, err := cli.ContainerCreate(ctx,
 		&container.Config{
 			Image: "node:16-alpine",
 			Cmd:   []string{"echo", "hello world"},
 		},
 		&container.HostConfig{
+			Resources: container.Resources{
+				CPUPeriod: 100000,
+				CPUQuota:  2000,
+			},
 			AutoRemove: true,
 		},
 		nil, nil, "")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("create container took %s \n", time.Since(start))
+	fmt.Printf("create container took %s \n", time.Since(timingCreateContainer))
 
+	timingStartContainer := time.Now()
 	if err := cli.ContainerStart(ctx, res.ID, types.ContainerStartOptions{}); err != nil {
 		panic(err)
 	}
-	fmt.Printf("start container took %s \n", time.Since(start))
+	fmt.Printf("start container took %s \n", time.Since(timingStartContainer))
 
 	fmt.Println(res)
+	statusCh, errCh := cli.ContainerWait(ctx, res.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, res.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
 }
